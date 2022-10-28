6e528537accb2d6a6dd45587831c30a8ca7f6900
shoamri joseph <shomarijoseph@gmail.com>
2022-10-28 16:51:38 -0400

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+const getComputerChoice = () => {
+  let num = Math.floor(Math.random() * 3) + 1;
+  switch (num) {
+    case 1:
+      return "Paper";
+      break;
+    case 2:
+      return "Scissors";
+      break;
+    case 3:
+      return "Rock";
+      break;
+    default:
+      return "error";
+  }
+};
+const playerSelection = choice => {
+switch  
+}
