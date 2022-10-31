405881bc5c54de014d41f66ed6907353837dc8f8
shoamri joseph <shomarijoseph@gmail.com>
2022-10-31 16:58:57 -0400

    redid algorithm for computer choice

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-const getComputerChoice = () => {
-  let num = Math.floor(Math.random() * 3) + 1;
-  switch (num) {
-    case 1:
-      return "Paper";
-      break;
-    case 2:
-      return "Scissors";
-      break;
-    case 3:
-      return "Rock";
-      break;
-    default:
-      return "error";
-  }
-};
-const playerSelection = choice => {
-switch  
+const choices = ["paper", "scissors", "rock"];
+const computerChoice = () => {
+  let computer = choices[Math.floor(Math.random() * 3)];
+  return computer;
 }
+
