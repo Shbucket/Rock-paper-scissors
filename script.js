16bda3ed2f5739061a9fd7e5c1d1c1c0a282fc5a
shoamri joseph <shomarijoseph@gmail.com>
2022-10-31 17:48:37 -0400

    add playgame function

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 const choices = ["paper", "scissors", "rock"];
 const computerChoice = () => {
-  let computer = choices[Math.floor(Math.random() * 3)];
-  return computer;
+ let pick = choices[Math.floor(Math.random() * 3)];
+ return pick;
+};
+const computer = computerChoice();
+const playerChoice = 'paper';
+const playGame = (computer, playerChoice) => {
+  if(playerChoice == computer){
+    return 'Tie';
+  } else { 
+    if (playerChoice == "rock") {
+      if (computer == "paper") {
+        return "Paper beats Rock sorry :(";
+      } else if (computer == "scissors") {
+        return "Rock beats scissors :)";
+      }
+    }
+     else if (playerChoice == "scissors") {
+      if (computer == "paper") {
+        return "Scissors beats paper :)";
+      } else if (computer == "rock") {
+        return "Rock beats scissors";
+      }
+    }
+     else if (playerChoice == "paper") {
+      if (computer == "rock") {
+        return "Paper beats rock :)";
+      } else if (computer == "scissors") {
+        return "Scissors beats paper :(";
+      }
+    } 
+  }
 }
-
