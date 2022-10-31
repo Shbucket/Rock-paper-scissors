53d580887231acbc186ba510fb7369443e3f2bcf
shoamri joseph <shomarijoseph@gmail.com>
2022-10-31 19:27:48 -0400

    tried fixing undefined error to no prevail

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 const choices = ["paper", "scissors", "rock"];
 const computerChoice = () => {
- let pick = choices[Math.floor(Math.random() * 3)];
- return pick;
+  let pick = choices[Math.floor(Math.random() * 3)];
+  return pick;
 };
+
 const computer = computerChoice();
-const playerChoice = 'paper';
+
+const playerChoice = "paper";
+
 const playGame = (computer, playerChoice) => {
-  if(playerChoice == computer){
-    return 'Tie';
-  } else { 
     if (playerChoice == "rock") {
+
       if (computer == "paper") {
         return "Paper beats Rock sorry :(";
       } else if (computer == "scissors") {
         return "Rock beats scissors :)";
       }
+
     }
      else if (playerChoice == "scissors") {
+
       if (computer == "paper") {
         return "Scissors beats paper :)";
       } else if (computer == "rock") {
-        return "Rock beats scissors";
+        return "Rock beats scissors :(";
       }
-    }
-     else if (playerChoice == "paper") {
+
+    } else if (playerChoice == "paper") {
+
       if (computer == "rock") {
         return "Paper beats rock :)";
       } else if (computer == "scissors") {
         return "Scissors beats paper :(";
       }
-    } 
-  }
-}
+
+    }
+
+     else {
+      return 'Its a draw' ;
+    }
+ 
+  
+};
