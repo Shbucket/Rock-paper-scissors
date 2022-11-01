9189e4eb986e971993d0c29f91fed32fcf358589
shoamri joseph <shomarijoseph@gmail.com>
2022-10-31 21:15:10 -0400

    added win counter for both computer and player

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 const choices = ["paper", "scissors", "rock"];
 const computerChoice = () => {
-  let pick = choices[Math.floor(Math.random() * 3)]
-  return pick
+  let pick = choices[Math.floor(Math.random() * 3)];
+  return pick;
 };
 
 const computer = computerChoice();
 
 const playerChoice = "paper";
+const computerWins = 0;
+const playerWins = 0;
 
 const playGame = (computer, playerChoice) => {
-  if (playerChoice == "rock") {
+  if (playerChoice == computer) {
+    return "Its a Draw!";
+  } else if (playerChoice == "rock") {
     if (computer == "paper") {
+      computerWins++;
       return "Paper beats Rock sorry :(";
     } else if (computer == "scissors") {
+      playerWins++;
       return "Rock beats scissors :)";
     }
   } else if (playerChoice == "scissors") {
     if (computer == "paper") {
       return "Scissors beats paper :)";
     } else if (computer == "rock") {
+      computerWins++;
       return "Rock beats scissors :(";
     }
   } else if (playerChoice == "paper") {
     if (computer == "rock") {
       return "Paper beats rock :)";
     } else if (computer == "scissors") {
+      computerWins++;
       return "Scissors beats paper :(";
     }
-  } else {
-    return "Its a draw";
   }
 };
 
-console.log(playGame(computer, playerChoice));
+/*console.log(playGame(computer, playerChoice));*/
