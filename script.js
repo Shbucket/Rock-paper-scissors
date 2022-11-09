c25956504751a3e2c378de62021ec4eb6d31681e
shoamri joseph <shomarijoseph@gmail.com>
2022-11-08 19:15:28 -0500

    rearranged code

--- script.js
+++ script.js
@@ -1,47 +1,3 @@
-const choices = ["paper", "scissors", "rock"];
-const computerChoice = () => {
-  let pick = choices[Math.floor(Math.random() * choices.length)];
-  computerDisplay.innerHTML = pick;
-};
-
-const computer = computerChoice();
-
-let playerChoice;
-let computerWins = 0;
-let playerWins = 0;
-/*const playGame = (computer, playerChoice) => {
-  if (playerChoice == computer) {
-    return "Its a Draw!";
-  } else if (playerChoice == "rock") {
-    if (computer == "paper") {
-      computerWins++;
-      return "Paper beats Rock sorry :(";
-    } else if (computer == "scissors") {
-      playerWins++;
-      return "Rock beats scissors :)";
-    }
-  } else if (playerChoice == "scissors") {
-    if (computer == "paper") {
-      playerWins++;
-      return "Scissors beats paper :)";
-    } else if (computer == "rock") {
-      computerWins++;
-      return "Rock beats scissors :(";
-    }
-  } else if (playerChoice == "paper") {
-    if (computer == "rock") {
-      playerWins++;
-      return "Paper beats rock :)";
-    } else if (computer == "scissors") {
-      computerWins++;
-      return "Scissors beats paper :(";
-    }
-  } else {
-    return "Error try again";
-  }
-};
-*/
-
 const paper = document.getElementById("paper");
 const rock = document.getElementById("rock");
 const scissors = document.getElementById("scissors");
@@ -49,13 +5,57 @@ const userDisplay = document.getElementById("userChoice");
 const computerDisplay = document.getElementById("computerPick");
 const result = document.getElementById("Winner");
 
+let computer;
+let playerChoice;
+let computerWins = 0;
+let playerWins = 0;
+
+const choices = ["paper", "scissors", "rock"];
+const computerChoice = () => {
+  let pick = choices[Math.floor(Math.random() * choices.length)];
+  computer = pick;
+  computerDisplay.innerHTML = pick;
+};
+
+const playGame = (computer, playerChoice) => {
+  if (playerChoice == computer) {
+    result.innerHTML = "Its a Draw!";
+  } else if (playerChoice == "rock") {
+    if (computer == "paper") {
+      computerWins++;
+      result.innerHTML = "Paper beats Rock sorry :(";
+    } else if (computer == "scissors") {
+      playerWins++;
+      result.innerHTML = "Rock beats scissors :)";
+    }
+  } else if (playerChoice == "scissors") {
+    if (computer == "paper") {
+      playerWins++;
+      result.innerHTML = "Scissors beats paper :)";
+    } else if (computer == "rock") {
+      computerWins++;
+      result.innerHTML = "Rock beats scissors :(";
+    }
+  } else if (playerChoice == "paper") {
+    if (computer == "rock") {
+      playerWins++;
+      result.innerHTML = "Paper beats rock :)";
+    } else if (computer == "scissors") {
+      computerWins++;
+      result.innerHTML = "Scissors beats paper :(";
+    }
+  } else {
+    result.innerHTML = "Error try again";
+  }
+};
+
 const handleClick = (e) => {
   playerChoice = e.target.id;
   userDisplay.innerHTML = playerChoice;
-  
+  computerChoice();
+  playGame(computer, playerChoice);
 };
 
 paper.addEventListener("click", handleClick);
 rock.addEventListener("click", handleClick);
 scissors.addEventListener("click", handleClick);
-
